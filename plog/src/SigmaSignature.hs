3f2c883716eb393574c32cb2eecec74d1763baa1
maple_workstation <maplevantablack@outlook.com>
2021-03-19 18:46:07 +0300

    added data types

--- plog/src/SigmaSignature.hs
+++ plog/src/SigmaSignature.hs
@@ -1,6 +1,30 @@
 module SigmaSignature
-  ( Individual,
+  ( Constant,
+    Variable,
+    Term,
+    Function,
+    Relation,
+    Formula,
   )
 where
 
-newtype Individual = Individual [Char]
+data Constant = A | B | C | D | E | F deriving (Show) -- predefined constant symbols
+
+data Variable = Variable [Char] deriving (Show) -- [Char] for variable name
+
+data Term = ConstTerm Constant | VarTerm Variable deriving (Show)
+
+data Function = Function ([Term] -> Term)
+
+data Relation = Relation [Char] Int deriving (Show) -- [Char] for relation name, Int for arity
+
+data Formula
+  = AtomicFormula Relation [Term]
+  | NOT Formula
+  | Formula `AND` Formula
+  | Formula `OR` Formula
+  | Formula `IMPLY` Formula
+  | Formula `EQUI` Formula
+  | FORALL Variable Formula --TODO: need to check this, according to definition Var should be free in Formula
+  | EXIST Variable Formula --TODO: need to check this
+  deriving (Show)
