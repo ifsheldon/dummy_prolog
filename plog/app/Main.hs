ccbba203f07e1fdee8d88b3507ac784822231f03
maple_workstation <maplevantablack@outlook.com>
2021-03-20 00:16:25 +0300

    tested CNF 1st and 2nd steps

--- plog/app/Main.hs
+++ plog/app/Main.hs
@@ -2,6 +2,12 @@ module Main where
 
 import Lib
 import SigmaSignature
+import Algorithms
 
-main :: IO ()
-main = someFunc
+main = do
+    let x = Variable "x"
+    let r1 = Relation "A" 1
+    let r2 = Relation "B" 1
+    let formula = IMPLY (QFormula FORALL x (AtomicFormula r1 [VarTerm x])) (QFormula EXIST x (AtomicFormula r2 [VarTerm x]))
+    let nf = negateFormula (stripArrows formula)
+    print (nf)
