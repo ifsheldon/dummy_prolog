5cba9a0efe2ce9110df7ce66218b77bd03ebf8d6
maple_workstation <maplevantablack@outlook.com>
2021-03-20 03:09:58 +0300

    seems finished standardize, unchecked

--- plog/src/Algorithms.hs
+++ plog/src/Algorithms.hs
@@ -7,6 +7,7 @@ where
 import SigmaSignature
 import Data.HashMap.Strict as HashMap
 import Data.Maybe
+import System.Exit
 ------------------ This part is for CNF conversion
 ------------------
 stripDoubleNot :: Formula -> Formula
@@ -54,7 +55,32 @@ replaceTerm mappings term = case term of
 replaceVarNameInTerms :: HashMap [Char] [Char] -> [Term] -> [Term]
 replaceVarNameInTerms nameMappings terms = Prelude.map (replaceTerm nameMappings) terms
 
-standardize :: Formula -> HashMap [Char] [Char] -> Int -> Formula 
-standardize formula nameMappings usedNameCount = case formula of 
-  AtomicFormula relation terms -> AtomicFormula relation (replaceVarNameInTerms nameMappings terms)
-  _ -> formula -- TODO
+checkVarNameAndUpdate :: ([Char], HashMap [Char] [Char], Int) -> ([Char], HashMap [Char] [Char], Int)
+checkVarNameAndUpdate (oldVarName, nameMappings, usedNameCount) = 
+  if oldVarName `member` nameMappings then
+    let newName = "v" ++ show usedNameCount
+        newMappings = insert oldVarName newName nameMappings
+    in
+      (newName, newMappings, usedNameCount + 1)
+  else
+    (oldVarName, nameMappings, usedNameCount)
+
+
+standardize :: (Formula, HashMap [Char] [Char], Int) -> (Formula, HashMap [Char] [Char], Int )
+standardize (formula, nameMappings, usedNameCount) = case formula of 
+  AtomicFormula relation terms -> (AtomicFormula relation (replaceVarNameInTerms nameMappings terms), nameMappings, usedNameCount)
+  NOT subformula -> standardize (subformula, nameMappings, usedNameCount)
+  AND sf0 sf1 -> (AND newsf0 newsf1, nameMappingsAfterStandardizeSf1, usedNameCountAfterStardardizeSf1) where
+    (newsf0, nameMappingsAfterStandardizeSf0, usedNameCountAfterStardardizeSf0) = standardize (sf0, nameMappings, usedNameCount)
+    (newsf1, nameMappingsAfterStandardizeSf1, usedNameCountAfterStardardizeSf1) = standardize (sf1, nameMappingsAfterStandardizeSf0, usedNameCountAfterStardardizeSf0)
+  OR sf0 sf1 -> (OR newsf0 newsf1, nameMappingsAfterStandardizeSf1, usedNameCountAfterStardardizeSf1) where
+    (newsf0, nameMappingsAfterStandardizeSf0, usedNameCountAfterStardardizeSf0) = standardize (sf0, nameMappings, usedNameCount)
+    (newsf1, nameMappingsAfterStandardizeSf1, usedNameCountAfterStardardizeSf1) = standardize (sf1, nameMappingsAfterStandardizeSf0, usedNameCountAfterStardardizeSf0)
+  QFormula quantifier (Variable varName) subformula -> (newQformula, newMapping, newCount) where
+    (newQformula, newMapping, newCount) = 
+      (QFormula quantifier (Variable newVarName) newSubFormula, 
+        mappingAfterStandardizeSubformula, 
+        countAfterStandardizeSubformula)
+      where
+      (newVarName, mappingAfterEvaluateVarName, countAfterEvaluateVarName) = checkVarNameAndUpdate (varName, nameMappings, usedNameCount)
+      (newSubFormula, mappingAfterStandardizeSubformula, countAfterStandardizeSubformula) = standardize (subformula, mappingAfterEvaluateVarName, countAfterEvaluateVarName)
