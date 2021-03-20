5611f8053375ad90f3e5275d3297c5451bb25185
maple_workstation <maplevantablack@outlook.com>
2021-03-20 04:45:37 +0300

    optimized code style, should not change logic

--- plog/src/Algorithms.hs
+++ plog/src/Algorithms.hs
@@ -17,15 +17,17 @@ stripDoubleNot formula = case formula of
 
 negateFormula :: Formula -> Formula --finished CNF 2.
 -- negateFormula formula = stripDoubleNot (NOT formula)
-negateFormula formula = case formula of 
-  NOT subformula -> stripDoubleNot(subformula)
-  st1 `OR` st2 -> stripDoubleNot(negateFormula st1 `AND` negateFormula st2)
-  st1 `AND` st2 -> stripDoubleNot(negateFormula st1 `OR` negateFormula st2)
-  st1 `IMPLY` st2 -> stripDoubleNot(negateFormula (stripArrows (st1 `IMPLY` st2)))
-  st1 `EQUIV` st2 -> stripDoubleNot(negateFormula (stripArrows (st1 `EQUIV` st2)))
-  QFormula FORALL var f -> QFormula EXIST var (stripDoubleNot(negateFormula f))
-  QFormula EXIST var f -> QFormula FORALL var (stripDoubleNot(negateFormula f))
-  _ -> stripDoubleNot (NOT formula)
+negateFormula formula = 
+  stripDoubleNot intermediate where
+    intermediate = case formula of 
+      NOT subformula -> subformula
+      st1 `OR` st2 -> negateFormula st1 `AND` negateFormula st2
+      st1 `AND` st2 -> negateFormula st1 `OR` negateFormula st2
+      st1 `IMPLY` st2 -> negateFormula (stripArrows (st1 `IMPLY` st2))
+      st1 `EQUIV` st2 -> negateFormula (stripArrows (st1 `EQUIV` st2))
+      QFormula FORALL var f -> QFormula EXIST var (negateFormula f)
+      QFormula EXIST var f -> QFormula FORALL var (negateFormula f)
+      _ -> NOT formula
 
 
 stripArrows :: Formula -> Formula -- finished CNF 1.
