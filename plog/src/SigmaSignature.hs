a759f358464587698ccbee4550d7259d6cd259bc
maple_workstation <maplevantablack@outlook.com>
2021-03-22 00:27:07 +0300

    added Eq for constructs

--- plog/src/SigmaSignature.hs
+++ plog/src/SigmaSignature.hs
@@ -15,28 +15,28 @@ module SigmaSignature
   )
 where
 
-data Constant = A | B | C | D | E | F | ExistConst [Char] deriving (Show) -- predefined constant symbols
+data Constant = A | B | C | D | E | F | ExistConst [Char] deriving (Show, Eq) -- predefined constant symbols
 
-data Variable = Variable {name_v :: [Char]}
+data Variable = Variable {name_v :: [Char]} deriving (Eq)
 instance Show Variable where
   show (Variable name) = "Var_"++name
 
-data Term = ConstTerm Constant | VarTerm Variable | FuncTerm Function [Term]
+data Term = ConstTerm Constant | VarTerm Variable | FuncTerm Function [Term] deriving (Eq)
 instance Show Term where
   show t = case t of 
     ConstTerm const -> show const
     VarTerm var -> show var
     FuncTerm f terms -> show f ++"("++show terms++")"
 
-data Function = Function {name_f :: [Char], arity_f :: Int}
+data Function = Function {name_f :: [Char], arity_f :: Int} deriving (Eq)
 instance Show Function where
   show (Function name _arity) = "Function_"++ name
 
-data Relation = Relation {name_r :: [Char], arity_r :: Int}
+data Relation = Relation {name_r :: [Char], arity_r :: Int} deriving (Eq)
 instance Show Relation where 
   show (Relation name _arity) = "Relation_"++ name
 
-data Quantifier = EXIST | FORALL deriving (Show)
+data Quantifier = EXIST | FORALL deriving (Show, Eq) 
 
 data Formula
   = AtomicFormula Relation [Term]
@@ -56,6 +56,17 @@ instance Show Formula where
     IMPLY f1 f2 -> "IMPLY (" ++ show f1 ++") ("++ show f2 ++ ")"
     EQUIV f1 f2 -> "EQUIV (" ++ show f1 ++") ("++ show f2 ++ ")"
     QFormula quantifier var f -> show quantifier ++ " {"++ show var ++"} ("++ show f ++ ")"
+
+instance Eq Formula where -- added place equality invariance compared to derived Eq
+  f1 == f2 = case (f1, f2) of
+    (AtomicFormula r1 terms1, AtomicFormula r2 terms2) -> r1 == r2 && terms1 == terms2
+    (NOT sf1, NOT sf2) -> sf1 == sf2
+    (AND sf11 sf12, AND sf21 sf22) -> (sf11 == sf21 && sf12 == sf22) || (sf11 == sf22 && sf12 == sf21)
+    (OR sf11 sf12, OR sf21 sf22) -> (sf11 == sf21 && sf12 == sf22) || (sf11 == sf22 && sf12 == sf21)
+    (EQUIV sf11 sf12, EQUIV sf21 sf22) -> (sf11 == sf21 && sf12 == sf22) || (sf11 == sf22 && sf12 == sf21)
+    (IMPLY sf11 sf12, IMPLY sf21 sf22) -> sf11 == sf21 && sf12 == sf22
+    (QFormula q1 v1 f1, QFormula q2 v2 f2) -> q1==q2 && v1==v2 && f1 == f2 --restricted version
+    _ -> False
 
 validateTerm :: Term -> Bool
 validateTerm term = case term of
