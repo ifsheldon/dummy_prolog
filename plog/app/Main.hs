bb5372da1b8e94da0b17111ae5036ffff85d2811
maple_workstation <maplevantablack@outlook.com>
2021-03-21 23:42:41 +0300

    naive test for distributing AND OR

--- plog/app/Main.hs
+++ plog/app/Main.hs
@@ -12,14 +12,19 @@ main = do
     let p = Relation "P" 3
     let q = Relation "Q" 3
     let formula = IMPLY (QFormula FORALL v0 (QFormula EXIST v1 (QFormula FORALL v2 (AtomicFormula p [VarTerm v0, VarTerm v1, VarTerm v2]) ))) (QFormula FORALL v1 (QFormula EXIST v2 (AtomicFormula q [VarTerm v1, VarTerm v2, VarTerm v0, VarTerm v2, VarTerm v0])))
+    print ("Original Formula: "++show formula) 
     let preprocessedFormula = stripArrows formula
+    print ("After eliminating arrows: " ++ show preprocessedFormula)
     let (standardizeFormula, _varRecord) = _standardize (preprocessedFormula,[] ,emptyVarRecord)
-    -- let formula = IMPLY (QFormula FORALL x (AtomicFormula r1 [VarTerm x])) (QFormula EXIST x (AtomicFormula r2 [VarTerm x]))
-    -- let nf = negateFormula (stripArrows formula)
-    -- print (nf)
-    print standardizeFormula
-    -- print _varRecord
+    print ("After standardization " ++ show standardizeFormula)
     let eliminatedFormula = eliminateExistentialInFormula standardizeFormula
-    print eliminatedFormula
+    print ("After eliminating existentials: "++ show eliminatedFormula)
     let noUniversalFormula = dropUniversals eliminatedFormula
-    print noUniversalFormula
+    print ("After dropping universals: "++ show noUniversalFormula)
+    print "\n Testing distribute AND OR----------"
+    let formulaA = AtomicFormula p [ConstTerm A]
+    let formulaB = AtomicFormula p [ConstTerm B]
+    let formulaC = AtomicFormula p [ConstTerm C]
+    let aAndBOrC = (formulaA `AND` formulaB) `OR` formulaC
+    print ("(A and B) or C: "++ show aAndBOrC)
+    print ("After distribution: "++ show (distributeANDOR aAndBOrC))
