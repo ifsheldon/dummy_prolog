5ad4f08e2af88e4a0cc6a533c98aa36c7a784253
maple_workstation <maplevantablack@outlook.com>
2021-03-26 00:50:13 +0300

    added Theory and corresponding functions

--- plog/src/Theories.hs
+++ plog/src/Theories.hs
@@ -4,5 +4,29 @@ module Theories
 where
 
 import SigmaSignature
+import Literals
+import Algorithms
 
-data Theory = Theory [Formula]
+data Theory = FormulaTheory [Formula] | ClauseTheory [Clause]
+
+fromFormulaTheoryToClauseTheory :: Theory -> Theory
+fromFormulaTheoryToClauseTheory formulaTheory = 
+  case formulaTheory of 
+    ClauseTheory _ -> formulaTheory
+    FormulaTheory formulas -> ClauseTheory $ concatMap getClausesFromFormula formulas
+
+validateFormulaTheory :: Signature -> Theory -> Bool 
+validateFormulaTheory signature theory = 
+  case theory of 
+    FormulaTheory formulas -> all (validateFormula signature) formulas
+
+queryWithTheory :: Theory -> Formula -> ResolveResult
+queryWithTheory theory query = 
+  let clausesInTheory = case theory of 
+                            ClauseTheory clauses -> clauses
+                            FormulaTheory formulas -> concatMap getClausesFromFormula formulas
+      negatedQuery = NOT query
+      queryClauses = getClausesFromFormula negatedQuery
+      allClauses = clausesInTheory ++ queryClauses
+  in
+    resolveClauses allClauses 
