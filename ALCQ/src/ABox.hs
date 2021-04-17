bd3de7e2b4b9e89f4edb5a102ec2e5aa1d4ca35c
Maple <feng.liang@kaust.edu.sa>
2021-04-17 18:11:01 +0300

    added hash for types

--- ALCQ/src/ABox.hs
+++ ALCQ/src/ABox.hs
@@ -19,9 +19,12 @@ instance Eq Individual where
 instance Hashable Individual where
   hashWithSalt salt individual =
     let (Individual _ interpretation) = individual
-     in hashWithSalt salt interpretation
+     in hashWithSalt salt interpretation -- FIXME: which field to hash?
 
 data Relation = Relation [Char] deriving (Show, Eq)
+
+instance Hashable Relation where
+  hashWithSalt salt relation = hashWithSalt salt name where (Relation name) = relation
 
 data Concept
   = Primitive [Char]
@@ -34,4 +37,23 @@ data Concept
   | Exist Relation Concept
   deriving (Show, Eq)
 
-data Assertion = RAssertion Relation Individual Individual | CAssertion Concept Individual
+instance Hashable Concept where
+  hashWithSalt salt concept =
+    let saltedHashC = hashWithSalt salt
+        saltedHashS = hashWithSalt salt
+     in case concept of
+          And c1 c2 -> saltedHashC c1 + saltedHashC c2
+          Or c1 c2 -> saltedHashC c1 + saltedHashC c2
+          Not c -> saltedHashS "Not" + saltedHashC c
+          Imply _ _ -> saltedHashS (show concept) -- TODO: fix this
+          Equiv c1 c2 -> saltedHashS "Equiv" + saltedHashC c1 + saltedHashC c2 -- TODO: fix this
+          Primitive name -> saltedHashS name
+          Exist r c -> hashWithSalt salt r + saltedHashC c + saltedHashS "Exist"
+          Forall r c -> hashWithSalt salt r + saltedHashC c + saltedHashS "Forall"
+
+data Assertion = RAssert Relation Individual Individual | CAssert Concept Individual deriving (Show, Eq)
+
+instance Hashable Assertion where
+  hashWithSalt salt assertion = case assertion of
+    RAssert r i1 i2 -> hashWithSalt salt r + hashWithSalt salt i1 + hashWithSalt salt i2 * 3
+    CAssert c i -> hashWithSalt salt c + hashWithSalt salt i
