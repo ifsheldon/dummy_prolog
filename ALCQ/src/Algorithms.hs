23924d7551e1a8456bfa1c4e5954b9897c531ccd
Maple <feng.liang@kaust.edu.sa>
2021-04-17 21:35:16 +0300

    added construction of ABR for later use

--- ALCQ/src/Algorithms.hs
+++ ALCQ/src/Algorithms.hs
@@ -4,6 +4,8 @@ module Algorithms
 where
 
 import ABox
+import Data.HashMap.Strict as HashMap
+import Data.HashSet as HashSet
 
 stripDoubleNot :: Concept -> Concept
 stripDoubleNot concept =
@@ -38,3 +40,54 @@ negateConcept concept = stripDoubleNot intermediate
       Forall r c -> Exist r (negateConcept c)
       Exist r c -> Forall r (negateConcept c)
       Primitive _ -> Not concept
+
+toNNFAssertion :: Assertion -> Assertion
+toNNFAssertion assertion =
+  case assertion of
+    RAssert _ _ _ -> assertion
+    CAssert concept individual -> CAssert (toNNF concept) individual
+
+data ABoxRecord = ABR
+  { relationMapping :: HashMap Relation (HashMap Individual (HashSet Individual)),
+    conceptAssertionList :: [Assertion]
+  }
+
+addAssertionToABR :: Assertion -> ABoxRecord -> ABoxRecord
+addAssertionToABR assertion abr =
+  case assertion of
+    RAssert relation i1 i2 ->
+      let relationMap = relationMapping abr
+          concept_assertion_list = conceptAssertionList abr
+          newRelationMap = case HashMap.lookup relation relationMap of
+            Nothing ->
+              -- if relation is not in relation map
+              let individualSet = HashSet.singleton i2
+                  individualMap = HashMap.singleton i1 individualSet
+               in HashMap.insert relation individualMap relationMap
+            Just individualMap ->
+              -- if relation in relation map
+              case HashMap.lookup i1 individualMap of
+                Nothing ->
+                  -- if i1 not in individual map
+                  let individualSet = HashSet.singleton i2
+                      newIndividualMap = HashMap.insert i1 individualSet individualMap
+                   in HashMap.insert relation newIndividualMap relationMap
+                Just individualSet ->
+                  -- if i1 in individual map
+                  let newIndividualSet = HashSet.insert i2 individualSet
+                      newIndividualMap = HashMap.insert i1 newIndividualSet individualMap
+                   in HashMap.insert relation newIndividualMap relationMap
+       in ABR newRelationMap concept_assertion_list
+    CAssert _ _ -> ABR (relationMapping abr) (assertion : conceptAssertionList abr)
+
+constructABRFromABox :: ABox -> ABoxRecord
+constructABRFromABox abox =
+  let Abox assertionSet = abox
+      nnfAssertionSet = HashSet.map toNNFAssertion assertionSet
+      emptyABR = ABR HashMap.empty []
+   in HashSet.foldr addAssertionToABR emptyABR assertionSet
+
+tableauAlgorithm :: ABox -> Bool
+tableauAlgorithm abox =
+  let aboxRecord = constructABRFromABox abox
+   in False --TODO
