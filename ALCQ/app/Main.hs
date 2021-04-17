bc0770ba7ad72c6eb26fac6c3b34ba2c18560a37
Maple <feng.liang@kaust.edu.sa>
2021-04-17 18:26:08 +0300

    added simple test

--- ALCQ/app/Main.hs
+++ ALCQ/app/Main.hs
@@ -1,5 +1,10 @@
 module Main where
-
+import ABox
+import Algorithms
 main :: IO ()
 main = do
-  print "hhhh"
+  let student = Primitive "Student"
+  let graduate = Primitive "Graduate"
+  let graduateStudent = And student graduate
+  let notGS = Not graduateStudent
+  print notGS
